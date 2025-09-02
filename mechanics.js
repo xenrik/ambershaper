9176aad86325c747be4bdbbb156695fe1389fde3
cloverpi <christopher.humber@gmail.com>
2025-09-02 13:44:59 -0600

    Youtube Links: Scottejaye

--- mechanics.js
+++ mechanics.js
@@ -27,6 +27,7 @@ const abilityImage = [
 const youtube = [
   { image: newImage('./images/joardee.webp'), link: 'https://www.youtube.com/watch?v=82s_NAFlKLw', text: `Joardee: Amber-Shaper Guide` },
   { image: newImage('./images/thegreatmetv.webp'), link: 'https://www.youtube.com/watch?v=NnaMCEOvdV8', text: `ThegreatmeTV: Construct Guide` },
+  { image: newImage('./images/scottejaye.webp'), link: 'https://www.youtube.com/watch?v=GU96CVodn74', text: `Scottejaye: Amber-Shaper Tips` },
   // { image: newImage('./images/scottejaye.webp'), link: 'https://youtube.com/scottejaye', text: `Scottejaye: Amber-Shaper Guide` },
 ];
 let youtubeIndex = 0;
